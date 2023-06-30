fd6482022427b9838e45b738e63678ba3894a628
unknown <bhotnikhil25@gmail.com>
2023-06-30 22:45:34 +0530

    [Nikhil]-Person Age Determination Problem

---
+++ PersonAgeDetermination/AgeCategoryException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonAgeDetermination
+{
+    public class AgeCategoryException : Exception
+    {
+        public AgeCategoryException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ PersonAgeDetermination/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonAgeDetermination
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AgeCategoryDeterminer ageDeterminer = new AgeCategoryDeterminer();
+
+            try
+            {
+                Console.Write("Enter a person's age: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new AgeCategoryException("Invalid age entered.");
+                }
+
+                int age = Convert.ToInt32(input);
+                string category = ageDeterminer.DetermineAgeCategory(age);
+                Console.WriteLine($"The person belongs to the category: {category}");
+            }
+            catch (AgeCategoryException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid age entered. Please enter a valid age.");
+            }
+        }
+    }
+}
